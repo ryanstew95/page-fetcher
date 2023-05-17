f3f756eb3897fa6010b3d28387ce86e7ed11f718
Ryan Stewart <ryanstew17@gmail.com>
2023-05-17 14:01:06 -0300

    finished app

--- fetcher.js
+++ fetcher.js
@@ -1,17 +1,22 @@
 const request = require('request');
-request('http://www.google.com', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+
 const fs = require('fs');
 
-const path = 'Some content!';
-const domain = 'filler text';
+const path = process.argv[3];
+const domain = process.argv[2];
 
-fs.writeFile('/Users/joe/test.txt', domain, err => {
-  if (err) {
-    console.error(err);
+request(domain, (error, response, body) => {
+  if (error) {
+    console.error(error);
   }
-  console.log(`Downloaded and saved ${domain} bytes to ${path}`);
+
+  // console.log('statusCode:', response && response.statusCode);
+
+  fs.writeFile(path, body, err => {
+    if (err) {
+      console.error(err);
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+  });
+
 });
